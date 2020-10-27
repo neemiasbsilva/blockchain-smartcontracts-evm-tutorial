472749f42445df23d1eea3a51b332febd5d647f9
neemiasbsilva <neemiasbsilva@gmail.com>
2020-10-27 10:47:28 -0400

    Change the value public

--- first-contract.sol
+++ first-contract.sol
@@ -1,15 +1,8 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    string value;
+    string public value = "myValue";
     
-    constructor() public{
-        value = "myValue";
-    }
-    
-    function get() public view returns(string memory){
-        return value;
-    }
     
     
     function set(string memory _value) public{
