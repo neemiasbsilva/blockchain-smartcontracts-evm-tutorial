157d6eb7f8c20ba3064c6803eba29f55985923ef
neemiasbsilva <neemiasbsilva@gmail.com>
2020-10-27 10:58:27 -0400

    Some types of solidity language

--- first-contract.sol
+++ first-contract.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.5.1;
-
+    
 contract MyContract {
-    string public constant value = "myValue";
+    string public string_value = "My string";
+    bool public bool_value = true;
+    int public int_value = 200;
+    uint public uint_value = 200;
+    uint8 public uint8_value = 8;
 }
