a0a46a37ca1d21943360c09ff4451331f18f6db5
neemiasbsilva <neemiasbsilva@gmail.com>
2020-10-27 10:49:56 -0400

    Change the value with constant

--- first-contract.sol
+++ first-contract.sol
@@ -1,10 +1,5 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    string public value = "myValue";
-    
-    
-    function set(string memory _value) public{
-        value = _value;
-    }
+    string public constant value = "myValue";
 }
