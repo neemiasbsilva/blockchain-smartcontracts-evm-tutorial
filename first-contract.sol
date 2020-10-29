86593002bc889afdffbe985f35e32ac38f91e1f5
neemiasbsilva <neemiasbsilva@gmail.com>
2020-10-29 19:08:19 -0400

    Mapping

--- first-contract.sol
+++ first-contract.sol
@@ -1,17 +1,21 @@
 pragma solidity 0.5.1;
     
 contract MyContract {
-    
-    Person[] public people;
     uint256 public peopleCount;
+    
+    mapping(uint => Person) public people;
+    
     struct Person {
+        uint _id;
         string _first_name;
         string _last_name;
     }
     
     function addPerson(string memory _first_name, string memory _last_name) public {
-        people.push(Person(_first_name, _last_name));
         peopleCount += 1;
+        people[peopleCount] = Person(peopleCount, _first_name, _last_name);
+        
     }
+    
     
 }
